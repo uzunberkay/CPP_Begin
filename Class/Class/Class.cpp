d5ad2297d3d7ffc9d6f1b117a40646486291d282
berkyuzun <berkyuzun@gmail.com>
2025-02-09 14:49:17 +0300

    Init

---
+++ Class/Class/Class.cpp
@@ -0,0 +1,15 @@
+﻿#include"studentClass.h"
+
+int main()
+{
+	Student student1("Berkay", "Uzun", "191305008");
+	student1.displayInformation();
+	student1.setStudentID("13174");
+	student1.displayInformation();
+
+	cout << student1.getStudentID() << endl;
+
+
+	
+}
+
